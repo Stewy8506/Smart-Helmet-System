77f2269954e1d2528fb5d6128135af15d070fef2
Anuvab Das <dasanuvab38@gmail.com>
2026-05-29 13:20:34 +0530

    fix pt.1

--- STM32 ANC Module/Core/Src/audio_pipeline.c
+++ STM32 ANC Module/Core/Src/audio_pipeline.c
@@ -1,6 +1,7 @@
 #include "audio_pipeline.h"
 #include "pin_config.h"
 #include "uart_parser.h"
+#include "main.h"
 #include <string.h>
 
 int32_t mic_rx_buffer[DMA_BUFFER_SIZE];
@@ -106,7 +107,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
         hdma_usart1_rx.Init.PeriphInc = DMA_PINC_DISABLE;
         hdma_usart1_rx.Init.MemInc = DMA_MINC_ENABLE;
         hdma_usart1_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-        hdma_usart1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+        hdma_usart1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
         hdma_usart1_rx.Init.Mode = DMA_CIRCULAR;
         hdma_usart1_rx.Init.Priority = DMA_PRIORITY_MEDIUM;
         hdma_usart1_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
